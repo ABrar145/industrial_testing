c53b498508b49622931e25da7acd3b5dba10e9a9
Amninder <amninderbrar04@gmail.com>
2026-01-22 15:40:51 -0600

    added page for frontend. we can make changes further

---
+++ industrial_testing/src/pages/UploadPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function UploadPage() {
+    return (
+        <div className="page">
+            <header></header>
+            <div>StorySpan</div>
+        </div>
+    );
+}
