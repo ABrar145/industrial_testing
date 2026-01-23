02dc187dff693a76aee23cb826b691c90f317841
Amninder <amninderbrar04@gmail.com>
2026-01-23 10:41:37 -0600

    added some style to page

--- industrial_testing/src/pages/UploadPage.jsx
+++ industrial_testing/src/pages/UploadPage.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import "./UploadPage.css";
 
 export default function UploadPage() {
 
@@ -37,7 +38,7 @@ export default function UploadPage() {
             
             </header>
 
-            <main>
+            <main className="page">
                 <h1>
                     Welcome back User!
                 </h1>
