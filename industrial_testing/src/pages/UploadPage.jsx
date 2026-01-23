d15e2db40f897d6080a5769d00a34421d45e99ec
Amninder <amninderbrar04@gmail.com>
2026-01-23 08:16:53 -0600

    front page

--- industrial_testing/src/pages/UploadPage.jsx
+++ industrial_testing/src/pages/UploadPage.jsx
@@ -3,8 +3,20 @@ import React from "react";
 export default function UploadPage() {
     return (
         <div className="page">
-            <header></header>
+            <header>
             <div>StorySpan</div>
+            
+            </header>
+
+            <main>
+                <h1>
+                    Welcome back User!
+                </h1>
+                <div>Upload video</div>
+                <div>
+                    <button>Upload</button>
+                </div>
+            </main>
         </div>
     );
 }
